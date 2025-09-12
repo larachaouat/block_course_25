6b2c98fb1c9831d426b1540e2f3eca10f2646d32
larachaouat <91006595+larachaouat@users.noreply.github.com>
2025-09-12 18:03:11 +0200

    app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ r = st.slider("Growth rate r", 0.01, 1.0, 0.3, 0.01)
 T = st.slider("Period T", 0.1, 5.0, 1.0, 0.1)
 I = st.slider("Removal fraction I (%)", 0, 90, 20, 5) / 100.0
 N0 = st.slider("Initial population N0", 1, 100, 10)
-n_steps = st.slider("Number of steps", 5, 100, 20)
+n_steps = st.slider("Number of steps", 0, 100, 20)
 
 times, pops = simulate(N0, K, r, T, I, n_steps)
 
